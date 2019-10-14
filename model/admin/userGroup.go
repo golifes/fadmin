fa3d40a94506d3e8f25d6a833965ce2043dd8a4c
95028555@qq.com <yangyi199159>
2019-10-14 09:58:33 +0800

    model更新加部分注释

--- model/admin/userGroup.go
+++ model/admin/userGroup.go
@@ -1,6 +1,11 @@
 package admin
 
-type userGroup struct {
-	Gid int64  `json:"gid"  `
-	Uid string `json:"uid"  `
+import "time"
+
+type UserGroup struct {
+	Gid    int64     `json:"gid"  `
+	Uid    string    `json:"uid"  `
+	Status int       `json:"status"  `
+	CTime  time.Time `json:"ctime"  `
+	MTime  time.Time `json:"mtime"  `
 }
