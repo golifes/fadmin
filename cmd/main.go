8026c37c8b1b3444bc77963a858ce5ac5bd1125a
95028555@qq.com <yangyi199159>
2019-10-16 17:11:03 +0800

    swag

--- cmd/main.go
+++ cmd/main.go
@@ -6,6 +6,20 @@ import (
 	"flag"
 )
 
+// @title Swagger Example API12222
+// @version 1.0
+// @description This is a sample server Petstore server.
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host petstore.swagger.io
+// @BasePath /v1
 func main() {
 	docs.SwaggerInfo.Schemes = []string{"http", "https"}
 	path := flag.String("-c", "config/config.json", "config.conf")
--- router/router.go
+++ router/router.go
@@ -1,10 +1,13 @@
 package router
 
 import (
+	_ "fadmin/cmd/docs"
 	a "fadmin/http/controller/admin"
 	"fadmin/http/middleware"
 	"fadmin/pkg/config"
 	"github.com/gin-gonic/gin"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	"github.com/swaggo/gin-swagger/swaggerFiles"
 )
 
 func InitRouter(path string) *Engine {
@@ -12,6 +15,8 @@ func InitRouter(path string) *Engine {
 		func() string { return config.NewHttpPort() }}
 	r.Use(middleware.DummyMiddleware())
 	admin(r)
+	r.GET("/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
 	//r.GET("/doc/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	r.Engine.Run(r.Port())
 	return r
