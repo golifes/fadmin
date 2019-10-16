078b23d67fd809a1ba5c85faead81bded9386899
95028555@qq.com <yangyi199159>
2019-10-16 15:03:10 +0800

    init swag

--- cmd/main.go
+++ cmd/main.go
@@ -5,6 +5,17 @@ import (
 	"flag"
 )
 
+// @title 后台管理api文档
+// @version 1.0
+// @description  Golang api of demo
+// @termsOfService http://github.com
+
+// @contact.name API Support
+// @contact.url http://www.cnblogs.com
+// @contact.email ×××@qq.com
+// @license.name MIT
+//@host 127.0.0.1:8081
+
 func main() {
 	path := flag.String("-c", "config/config.json", "config.conf")
 	router.InitRouter(*path)
